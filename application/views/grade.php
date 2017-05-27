6ead3161e077edeefa65eb2b769b482470f37339
Your Name <TahjidEshan>
2017-05-27 12:02:36 +0600

    Not as fast as I thought

--- application/views/grade.php
+++ application/views/grade.php
@@ -51,7 +51,8 @@
             </header>
             <div id="sidebar-wrapper" style="height:100%;">
                 <ul id="sidebar-nav" style="list-style:none">
-                    <li class="sidebar-brand">Easy to Find</li>
+                    <li class="sidebar-brand"><img id="sidebar-image" class="img-responsive" src="" alt="COULD NOT LOAD IMAGE"></li>
+                    <li class="sidebar-brand">Easy to Find    <a onclick="closeNav()" class="close" aria-label="Close"> <span aria-hidden="true">&times;</span></a></li>
                     <li class="sidebar-brand"><div id="easytofind" class="rating-passive" data-rating=""><a class="stars"></a></div></li>
                     <li class="sidebar-brand">Accesibility</li>
                     <li class="sidebar-brand"><div id="accesibility"class="rating-passive" data-rating=""><a class="stars"></a></div></li>
@@ -63,15 +64,15 @@
                     <li class="sidebar-brand"><div id="food" class="rating-passive" data-rating=""><a class="stars"></a></div></li>
                     <li class="sidebar-brand">Noise Level</li>
                     <li class="sidebar-brand"><div id="noise" class="rating-passive" data-rating=""><a class="stars"></a></div></li>
-                    <li class="sidebar-brand"><a onclick="closeNav()">Go Back</a></li>
                 </ul>
+                <a onclick="closeNav()">Go Back</a>
             </div>
             <div id="page-content">
                 <div id="main">
                     <div class="firstPage" style="background-color:#4D4D70; height:85vh; margin-bottom:10vh;padding-top:30vh;">
                         <h1 style="color:#FFFFFF; text-align:center; font-size:54px;margin-top: 5px;margin-bottom: 10px;font-family:Tahoma,Verdana,sans-serif;line-height: 1.1; font-weight:bold;">Welcome to Grade</h1>
                         <h2 style="color:white,opacity: 0.5; text-align:center;color: #fff;font-size: 24px;font-weight: normal;margin-top: 5px;margin-bottom: 10px;font-family: font-family: Tahoma,Verdana,sans-serif;line-height: 1.1;">
-                        Grade is a universal social accessibility tool for people with disabilities. We map and grade accessible places around the country to promote equal access for everyone. We share our finding with everyone. We orgnize and ask public officials to make changes, most importanly, we go out and have fun. Grade is run by the awesome people at Cidny
+                            Grade is a universal social accessibility tool for people with disabilities. We map and grade accessible places around the country to promote equal access for everyone. We share our finding with everyone. We orgnize and ask public officials to make changes, most importanly, we go out and have fun. Grade is run by the awesome people at Cidny
                         </h2>
                     </div>
                     <!-- map -->
@@ -92,88 +93,96 @@
                                         <span class="input-group-btn">
                                             <button class="btn btn-default" type="submit"><i class="arrow_right"></i></button>
                                         </span>
-                                        </div><!-- /input-group -->
-                                    </form>
-                                    <!--end form-->
-                                </div>
-                                <!--end search-form-->
+                                    </div><!-- /input-group -->
+                                </form>
+                                <!--end form-->
                             </div>
+                            <!--end search-form-->
                         </div>
-                        <!--end hero-section-->
-                        <section class="block">
-                            <div class="container col-md-12 col-sm-12 col-xs-12">
-                                <div class="center">
-                                    <div class="section-title">
-                                        <div class="center">
-                                            <button class="btn btn-default">Cafe</button>
-                                        </div>
+                    </div>
+                    <!--end hero-section-->
+                    <section class="block">
+                        <div class="container col-md-12 col-sm-12 col-xs-12">
+                            <div class="center">
+                                <div class="section-title">
+                                    <div class="center">
+                                        <button class="btn btn-default">Cafe</button>
                                     </div>
-                                    <!--end section-title-->
                                 </div>
-                                <!--end center-->
-                                <div class="row ">
-                                    <?php foreach ($cafe as $value): $id=$value->cafe_survey_common_id;
-                                    $easytodfind=0;
-                                    $accesibility=0;
-                                    $ambiance=0;
-                                    $staff=0;
-                                    $food=0;
-                                    $noise=0;
-                                    $img ="https://maps.googleapis.com/maps/api/streetview?size=640x640&location=<?php echo $value->lat;?>,<?php echo $value->lng;?>7&key=AIzaSyBaDWhE5AeN2ar9Nz1bqDvzNQWJcj-iqjU";
+                                <!--end section-title-->
+                            </div>
+                            <!--end center-->
+                            <div class="row ">
+                                <?php
+                                foreach ($cafe as $value): $id = $value->cafe_survey_common_id;
+                                    $easytodfind = 0;
+                                    $accesibility = 0;
+                                    $ambiance = 0;
+                                    $staff = 0;
+                                    $food = 0;
+                                    $noise = 0;
+                                    $img = "https://maps.googleapis.com/maps/api/streetview?size=640x640&location=" . $value->lat . "," . $value->lng . "7&key=AIzaSyBaDWhE5AeN2ar9Nz1bqDvzNQWJcj-iqjU";
                                     foreach ($cafeRating as $rating):
-                                    if ($rating->cafe_survey_common_id == $id) {
-                                        if($rating->grade_type_id == 1){
-                                            $easytofind=$rating->rating;
-                                        }else if($rating->grade_type_id == 2){
-                                            $accesibility=$rating->rating;
-                                        }else if($rating->grade_type_id == 3){
-                                            $ambiance=$rating->rating;
-                                        }else if($rating->grade_type_id == 4){
-                                            $staff=$rating->rating;
-                                        }else if($rating->grade_type_id == 5){
-                                            $food=$rating->rating;
-                                        }else if($rating->grade_type_id == 6){
-                                            $noise=$rating->rating;
+                                        if ($rating->cafe_survey_common_id == $id) {
+                                            if ($rating->grade_type_id == 1) {
+                                                $easytofind = $rating->rating;
+                                            } else if ($rating->grade_type_id == 2) {
+                                                $accesibility = $rating->rating;
+                                            } else if ($rating->grade_type_id == 3) {
+                                                $ambiance = $rating->rating;
+                                            } else if ($rating->grade_type_id == 4) {
+                                                $staff = $rating->rating;
+                                            } else if ($rating->grade_type_id == 5) {
+                                                $food = $rating->rating;
+                                            } else if ($rating->grade_type_id == 6) {
+                                                $noise = $rating->rating;
+                                            }
                                         }
-
-                                    }
                                     endforeach;
                                     ?>
-
+                                    <script>
+                                        var easytofind = "<?php echo $easytofind; ?>";
+                                        var accesibility = "<?php echo $accesibility; ?>";
+                                        var ambiance = "<?php echo $ambiance; ?>";
+                                        var staff = "<?php echo $staff; ?>";
+                                        var food = "<?php echo $food; ?>";
+                                        var noise = "<?php echo $noise; ?>";
+                                        var img = "<?php echo $img; ?>";
+                                    </script>
                                     <div class="itemTab col-md-4 col-sm-4 col-xs-4">
                                         <div class="item">
-                                            <a  onclick="openNav()">
+                                            <a  onclick="openNav(easytofind, accesibility, ambiance, staff, food, noise, img)">
                                                 <div class="description">
-                                                <figure></figure>
-                                                <div class="label label-default">Cafe</div>
-                                                <h3><?php echo $value->name; ?></h3>
-                                                <h4><?php echo $value->address; ?></h4>
-                                                <?php echo $value->city; ?>
-                                             </div>
-                                            <!--end description-->
-                                            <div class="image bg-transfer">
-                                                <img src="https://maps.googleapis.com/maps/api/streetview?size=640x640&location=<?php echo $value->lat;?>,<?php echo $value->lng;?>7&key=AIzaSyBaDWhE5AeN2ar9Nz1bqDvzNQWJcj-iqjU" alt="">
+                                                    <figure></figure>
+                                                    <div class="label label-default">Cafe</div>
+                                                    <h3><?php echo $value->name; ?></h3>
+                                                    <h4><?php echo $value->address; ?></h4>
+    <?php echo $value->city; ?>
+                                                </div>
+                                                <!--end description-->
+                                                <div class="image bg-transfer">
+                                                    <img class="img-responsive" src="https://maps.googleapis.com/maps/api/streetview?size=640x640&location=<?php echo $value->lat; ?>,<?php echo $value->lng; ?>7&key=AIzaSyBaDWhE5AeN2ar9Nz1bqDvzNQWJcj-iqjU" alt="">
+                                                </div>
+                                                <!--end image-->
+                                            </a>
+                                            <div class="additional-info">
+                                                <div class="rating-passive" data-rating="">
+                                                    <span class="stars"></span>
+                                                </div>
+                                                <div class="controls-more">
+                                                    <ul>
+                                                        <li><a onclick="openNav(easytofind, accesibility, ambiance, staff, food, noise, img)">Details</a></li>
+                                                        <li><a href="#">Show On Map</a></li>
+                                                        <li><a href="#">Get Direction</a></li>
+                                                    </ul>
+                                                </div>
+                                                <!--end controls-more-->
                                             </div>
-                                            <!--end image-->
-                                        </a>
-                                        <div class="additional-info">
-                                            <div class="rating-passive" data-rating="">
-                                                <span class="stars"></span>
-                                            </div>
-                                            <div class="controls-more">
-                                                <ul>
-                                                    <li><a onclick="openNav('<?php echo $easytofind; ?>', '<?php echo $accesibility; ?>','<?php echo $ambiance; ?>','<?php echo $staff; ?>',' <?php echo$food; ?>',' <?php echo $noise; ?>' ,'<?php echo $img; ?>')">Details</a></li>
-                                                    <li><a href="#">Show On Map</a></li>
-                                                    <li><a href="#">Get Direction</a></li>
-                                                </ul>
-                                            </div>
-                                            <!--end controls-more-->
+                                            <!--end additional-info-->
                                         </div>
-                                        <!--end additional-info-->
+                                        <!--end item-->
                                     </div>
-                                    <!--end item-->
-                                </div>
-                                <?php endforeach; ?>
+<?php endforeach; ?>
                             </div>
                             <!--end row-->
                             <!--end center-->
@@ -224,36 +233,37 @@
                 <!--end page-wrapper-->
                 <a href="#" class="to-top scroll" data-show-after-scroll="600"><i class="arrow_up"></i></a>
                 <script>
-                var optimizedDatabaseLoading = 0;
-                var _latitude = 40.732714;
-                var _longitude = -73.991393;
-                var element = "map-homepage";
-                var markerTarget = "modal"; // use "sidebar", "infobox" or "modal" - defines the action after click on marker
-                var sidebarResultTarget = "modal"; // use "sidebar", "modal" or "new_page" - defines the action after click on marker
-                var showMarkerLabels = false; // next to every marker will be a bubble with title
-                var mapDefaultZoom = 12; // default zoom
-                heroMap(_latitude, _longitude, element, markerTarget, sidebarResultTarget, showMarkerLabels, mapDefaultZoom);
+                    var optimizedDatabaseLoading = 0;
+                    var _latitude = 40.732714;
+                    var _longitude = -73.991393;
+                    var element = "map-homepage";
+                    var markerTarget = "modal"; // use "sidebar", "infobox" or "modal" - defines the action after click on marker
+                    var sidebarResultTarget = "modal"; // use "sidebar", "modal" or "new_page" - defines the action after click on marker
+                    var showMarkerLabels = false; // next to every marker will be a bubble with title
+                    var mapDefaultZoom = 12; // default zoom
+                    heroMap(_latitude, _longitude, element, markerTarget, sidebarResultTarget, showMarkerLabels, mapDefaultZoom);
                 </script>
             </div>
         </div>
         <script>
-        function openNav(easytofind,accesibility,ambiance, staff, food, noise,img) {
-        document.getElementById("sidebar-wrapper").style.width = "30%";
-        document.getElementById("main").style.marginLeft = "30vw";
-        document.getElementById("main").style.width = "70vw";
-        $('.itemTab').removeClass("col-md-4 col-xs-4 col-sm-4").addClass("col-md-3 col-xs-3 col-sm-3");
-        $('.container').removeClass("col-md-12 col-xs-12 col-sm-12").addClass("col-md-11 col-xs-11 col-sm-11");
-        $( "#easytofind" ).attr( "data-rating", easytofind);
-        window.alert(easytofind);
-        }
-        /* Set the width of the side navigation to 0 */
-        function closeNav() {
-        document.getElementById("sidebar-wrapper").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-        document.getElementById("main").style.width = "100vw";
-        $('.itemTab').removeClass("col-md-3 col-xs-3 col-sm-3").addClass("col-md-4 col-xs-4 col-sm-4");
-        $('.container').removeClass("col-md-11 col-xs-11 col-sm-11").addClass("col-md-12 col-xs-12 col-sm-12");
-        }
+            function openNav(easytofind, accesibility, ambiance, staff, food, noise, img) {
+                closeNav();
+                document.getElementById("sidebar-wrapper").style.width = "30%";
+                document.getElementById("main").style.marginLeft = "30vw";
+                document.getElementById("main").style.width = "70vw";
+                $('.itemTab').removeClass("col-md-4 col-xs-4 col-sm-4").addClass("col-md-3 col-xs-3 col-sm-3");
+                $('.container').removeClass("col-md-12 col-xs-12 col-sm-12").addClass("col-md-11 col-xs-11 col-sm-11");
+                $("#sidebar-image").attr("src", img);
+                // window.alert();
+            }
+            /* Set the width of the side navigation to 0 */
+            function closeNav() {
+                document.getElementById("sidebar-wrapper").style.width = "0";
+                document.getElementById("main").style.marginLeft = "0";
+                document.getElementById("main").style.width = "100vw";
+                $('.itemTab').removeClass("col-md-3 col-xs-3 col-sm-3").addClass("col-md-4 col-xs-4 col-sm-4");
+                $('.container').removeClass("col-md-11 col-xs-11 col-sm-11").addClass("col-md-12 col-xs-12 col-sm-12");
+            }
         </script>
     </div>
 </body>
